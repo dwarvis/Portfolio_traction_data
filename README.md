e7e14467d10cea737ce25a6da2d3e2acf33c3139
davischeng <davischeng77@gmail.com>
2022-09-15 14:05:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+dependencies:
+cachetools               5.2.0
+certifi                  2022.6.15
+cffi                     1.15.1
+charset-normalizer       2.1.1
+cryptography             38.0.0
+google-api-core          2.10.0
+google-api-python-client 2.60.0
+google-auth              2.11.0
+google-auth-httplib2     0.1.0
+google-auth-oauthlib     0.5.2
+googleapis-common-protos 1.56.4
+gspread                  5.5.0
+httplib2                 0.20.4
+idna                     3.3
+oauth2client             4.1.3
+oauthlib                 3.2.0
+pip                      21.3.1
+protobuf                 4.21.5
+pyasn1                   0.4.8
+pyasn1-modules           0.2.8
+pycoingecko              3.0.0
+pycparser                2.21
+pyOpenSSL                22.0.0
+pyparsing                3.0.9
+PyYAML                   6.0
+requests                 2.28.1
+requests-oauthlib        1.3.1
+rsa                      4.9
+searchtweets             1.7.6
+setuptools               60.2.0
+six                      1.16.0
+tweet-parser             1.13.2
+uritemplate              4.1.1
+urllib3                  1.26.12
+wheel                    0.37.1
+
---
+++ main.py
@@ -0,0 +1,169 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from pycoingecko import CoinGeckoAPI
+import requests
+import os
+import json
+
+# Twitter Bearer Token for @intern_davisc
+bearer_token = "AAAAAAAAAAAAAAAAAAAAANAhhAEAAAAA0azzTujSXV1w58A0x26V1cfvzh8%3DPhdd95xnQvrn6d8vPixNGno0fEwN9Y7vKVpjW37sThipY2reqt"
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1rGBynD_Z8ML2bcxQOgRWVpSfKSBT7LrPs5uOVygmXcw'
+SAMPLE_RANGE_NAME = 'A1:E10'
+
+cg = CoinGeckoAPI()
+
+
+def create_url(users):
+    # Specify the usernames that you want to lookup below
+    # You can enter up to 100 comma-separated values.
+    # EXAMPLE: usernames = "usernames=TwitterDev,TwitterAPI"
+    usernames = "usernames=" + users
+    user_fields = "user.fields=public_metrics"
+    # User fields are adjustable, options include:
+    # created_at, description, entities, id, location, name,
+    # pinned_tweet_id, profile_image_url, protected,
+    # public_metrics, url, username, verified, and withheld
+    url = "https://api.twitter.com/2/users/by?{}&{}".format(usernames, user_fields)
+    return url
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2UserLookupPython"
+    return r
+
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth,)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+def update_values(creds, range_name, value_input_option,
+                  _values):
+    """
+    Creates the batch_update the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+        """
+    # pylint: disable=maybe-no-member
+    try:
+
+        service = build('sheets', 'v4', credentials=creds)
+        values = [
+            [
+                # Cell values ...
+            ],
+            # Additional rows ...
+        ]
+        body = {
+            'values': _values
+        }
+        result = service.spreadsheets().values().update(
+            spreadsheetId=SPREADSHEET_ID, range=range_name,
+            valueInputOption=value_input_option, body=body).execute()
+        print(f"{result.get('updatedCells')} cells updated.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
+def get_twitter_followers(users):
+    try:
+        url = create_url(users)
+        json_response = connect_to_endpoint(url)
+        print(json.dumps(json_response, indent=4, sort_keys=True))
+    except:
+        return 'n/a'
+
+
+def get_token_ticker(tid):
+    try:
+        return cg.get_coin_by_id(tid)['tickers'][0]['base']
+    except:
+        return 'n/a'
+
+
+def get_token_address(tid):
+    try:
+        return cg.get_coin_by_id(tid)['contract_address']
+    except:
+        return 'n/a'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/Users/davischeng/Desktop/client_secret_491554625934-rr7gfbt99rp0utp3n2vlcdv3iu84h0mk.apps.googleusercontent.com.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print(values)
+
+    except HttpError as err:
+        print(err)
+
+    # TODO: make it write the relevant data
+    update_values(creds, "A1:C2", "USER_ENTERED",
+                  [
+                      ['A', 'B'],
+                      ['C', 'D']
+                  ])
+
+
+if __name__ == '__main__':
+    main()
+
